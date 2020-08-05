cbef24410d4e099b6cca49dc58b1511fdb011016
zhangjyu88 <54967705+zhangjyu88@users.noreply.github.com>
2020-08-06 00:01:30 +0800

    Update truffle.js

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const mnemonic = "magic burger accuse sudden book stamp easy dial sting exile promote vanish";
+const mnemonic = ".....";
 
 module.exports = {
   networks: {
@@ -9,11 +9,11 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     rinkeby:{
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/b69db4c588ef4f9b812c87eaf1f5d0af`),
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/......`),
       network_id: 4,
       gas: 4500000,
       gasPrice: 10000000000,
-      from: "0xf37745461FB7320c3cB2a0Cd9888c08a9326c145"
+      from: "......"
     }
   },
   mocha: {
